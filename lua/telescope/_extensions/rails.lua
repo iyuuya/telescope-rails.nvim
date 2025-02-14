515eff165009426cec8662fdfa86b2c08a995eb1
iyuuya <i.yuuya@gmail.com>
2025-02-14 17:11:44 +0900

    feat: add finders

--- lua/telescope/_extensions/rails.lua
+++ lua/telescope/_extensions/rails.lua
@@ -27,29 +27,29 @@ end
 local find_rails = function(target, target_path, opts)
   opts = opts or {}
   pickers
-    .new(opts, {
-      prompt_title = target,
-      finder = finders.new_oneshot_job({
-        "find",
-        target_path,
-        "-type",
-        "f",
-      }, {
-        entry_maker = function(file)
-          local path_without_prefix = path_to_display_name(file, target_path)
-          return {
-            value = file,
-            display = function(table)
-              return make_display(table)
-            end,
-            ordinal = path_without_prefix,
-            path = file,
-          }
-        end,
-      }),
-      sorter = conf.generic_sorter(opts),
-    })
-    :find()
+      .new(opts, {
+        prompt_title = target,
+        finder = finders.new_oneshot_job({
+          "find",
+          target_path,
+          "-type",
+          "f",
+        }, {
+          entry_maker = function(file)
+            local path_without_prefix = path_to_display_name(file, target_path)
+            return {
+              value = file,
+              display = function(table)
+                return make_display(table)
+              end,
+              ordinal = path_without_prefix,
+              path = file,
+            }
+          end,
+        }),
+        sorter = conf.generic_sorter(opts),
+      })
+      :find()
 end
 
 local find_models = function(opts)
@@ -76,6 +76,12 @@ local find_views = function(opts)
   find_rails("views", "app/views", opts)
 end
 
+function gen_find_app_fn(dir)
+  return function(opts)
+    find_rails(dir, "app/" + dir, opts)
+  end
+end
+
 return require("telescope").register_extension {
   exports = {
     models = find_models,
@@ -84,5 +90,30 @@ return require("telescope").register_extension {
     migrations = find_migrations,
     specs = find_specs,
     views = find_views,
+
+    assets = function(opts) find_rails("assets", "app/assets", opts) end,
+    batches = function(opts) find_rails("batches", "app/batches", opts) end,
+    callbacks = function(opts) find_rails("callbacks", "app/callbacks", opts) end,
+    channels = function(opts) find_rails("channels", "app/channels", opts) end,
+    components = function(opts) find_rails("components", "app/components", opts) end,
+    decorators = function(opts) find_rails("decorators", "app/decorators", opts) end,
+    finders = function(opts) find_rails("finders", "app/finders", opts) end,
+    forms = function(opts) find_rails("forms", "app/forms", opts) end,
+    helpers = function(opts) find_rails("helpers", "app/helpers", opts) end,
+    javascript = function(opts) find_rails("javascript", "app/javascript", opts) end,
+    jobs = function(opts) find_rails("jobs", "app/jobs", opts) end,
+    mailers = function(opts) find_rails("mailers", "app/mailers", opts) end,
+    policies = function(opts) find_rails("policies", "app/policies", opts) end,
+    queries = function(opts) find_rails("queries", "app/queries", opts) end,
+    serializers = function(opts) find_rails("serializers", "app/serializers", opts) end,
+    services = function(opts) find_rails("services", "app/services", opts) end,
+    validators = function(opts) find_rails("validators", "app/validators", opts) end,
+    view_models = function(opts) find_rails("view_models", "app/view_models", opts) end,
+
+    graphql = function(opts) find_rails("graphql", "app/graphql", opts) end,
+    graphql_mutations = function(opts) find_rails("graphql mutations", "app/graphql/mutations", opts) end,
+    graphql_resolvers = function(opts) find_rails("graphql resolvers", "app/graphql/resolvers", opts) end,
+    graphql_sources = function(opts) find_rails("graphql sources", "app/graphql/sources", opts) end,
+    graphql_types = function(opts) find_rails("graphql types", "app/graphql/types", opts) end,
   },
 }
